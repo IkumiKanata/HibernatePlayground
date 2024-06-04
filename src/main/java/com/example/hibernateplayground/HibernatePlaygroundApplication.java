91bea6373624d91013ba4774d397588d925be7eb
KanataIkumi <i.kanata0531@gmail.com>
2024-06-04 11:05:03 +0900

    Add commandline runner

--- src/main/java/com/example/hibernateplayground/HibernatePlaygroundApplication.java
+++ src/main/java/com/example/hibernateplayground/HibernatePlaygroundApplication.java
@@ -1,13 +1,32 @@
 package com.example.hibernateplayground;
 
+import com.example.hibernateplayground.dao.AppDAO;
+import com.example.hibernateplayground.entity.Instructor;
+import com.example.hibernateplayground.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernatePlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernatePlaygroundApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(HibernatePlaygroundApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+            createInstructor(appDAO);
+		};
 	}
 
+    private void createInstructor(AppDAO appDAO) {
+        Instructor instructor = new Instructor("Chad", "Darby", "test.com");
+
+        InstructorDetail instructorDetail = new InstructorDetail("https://sample.com", "Luv 2 code!!!");
+        instructor.setInstructorDetail(instructorDetail);
+        appDAO.save(instructor);
+    }
 }
