221af77c3a478639807aeb3a363cee50ef8f92be
KanataIkumi <i.kanata0531@gmail.com>
2024-06-04 11:04:33 +0900

    DAO追加

---
+++ src/main/java/com/example/hibernateplayground/dao/AppDAO.java
@@ -0,0 +1,7 @@
+package com.example.hibernateplayground.dao;
+
+import com.example.hibernateplayground.entity.Instructor;
+
+public interface AppDAO {
+    public void save(Instructor instructor);
+}
