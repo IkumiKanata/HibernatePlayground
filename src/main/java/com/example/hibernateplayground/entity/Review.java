ee83101718729baba0c848b682b20cfdd1fd3ca0
KanataIkumi <i.kanata0531@gmail.com>
2024-06-24 21:09:44 +0900

    add Review

---
+++ src/main/java/com/example/hibernateplayground/entity/Review.java
@@ -0,0 +1,34 @@
+package com.example.hibernateplayground.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    private String comment;
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+
+}
