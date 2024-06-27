622339bc63fea4f941832ff541f4419bbee42fd3
KanataIkumi <i.kanata0531@gmail.com>
2024-06-27 22:59:29 +0900

    add student entity

--- src/main/java/com/example/hibernateplayground/entity/Student.java
+++ src/main/java/com/example/hibernateplayground/entity/Student.java
@@ -14,10 +14,13 @@ import lombok.Setter;
 @Setter
 public class Student {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
     private String email;
 
